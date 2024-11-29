54237661428d5f5819d5199b543b5d26d56055e8
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-11-29 13:59:29 +0100

    Feat : (Form1.cs) Création méthode "GetAllFileInfosInPath
    60m-DONE
    Cette méthode va récupérer tout les infos des fichiers mp3 dans un répertoire choisis, et pour chacuns en crée un objet.

--- P_BitRuisseau/Form1.cs
+++ P_BitRuisseau/Form1.cs
@@ -1,14 +1,18 @@
 using TagLib;
 using System.IO;
 using System.Reflection;
+using TagLib.Flac;
 namespace P_BitRuisseau
 {
     public partial class Form1 : Form
     {
+        List<MediaData> mediaDatas = new List<MediaData>();
+        public string mediasPath = "../../../ressource/";
         public Form1()
         {
             InitializeComponent();
-
+            mediaDatas = GetAllFileInfosInPath(mediasPath);
+            updateListeFichiersLocaux(mediaDatas);
         }
 
         private void UpLoadFichier_Click(object sender, EventArgs e)
@@ -21,7 +25,7 @@ namespace P_BitRuisseau
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     string selectedFilePath = openFileDialog.FileName;
-                    
+
                     MessageBox.Show($"Fichier sélectionné : {selectedFilePath}", "Fichier MP3");
                     try
                     {
@@ -36,14 +40,15 @@ namespace P_BitRuisseau
                         //MessageBox.Show("title : " + title +"Artiste : " +  artist + "FileType : " + fileType + "Size : "+ size+ "Duration : " + duration);
                         //MediaData mediaData = new MediaData(title,artist,fileType, size, duration);
                         //addFileIntoListeLocal(mediaData);
-                    } catch(Exception ex)
+                    }
+                    catch (Exception ex)
                     {
                         MessageBox.Show($"Erreur lors de la lecture du fichier MP3 : {ex.Message}", "Erreur");
                     }
-                }  
+                }
             }
         }
-    
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -66,9 +71,34 @@ namespace P_BitRuisseau
         {
             string[] Files = Directory.GetFiles("../../../ressource/", "*.*");
         }
-        public void getAllFileInPath(string path)
+        static public List<MediaData> GetAllFileInfosInPath(string folderPath)
         {
+            List<MediaData> mediaDatas = new List<MediaData>();
+
+            string[] mediaFiles = Directory.GetFiles(folderPath, "*.mp3");
+            foreach (string mediaFile in mediaFiles)
+            {
+                if (System.IO.File.Exists(mediaFile))
+                {
+                    FileInfo fileInfo = new FileInfo(mediaFile);
+
+                    var file = TagLib.File.Create(mediaFile);
+                    string title = file.Tag.Title ?? "Inconnu";
+                    string artist = file.Tag.FirstPerformer ?? "Inconnu";
+                    string fileType = fileInfo.Extension.ToLower();
+                    long size = fileInfo.Length / 1024;
+                    string duration = file.Properties.Duration.ToString(@"mm\:ss");
+                    MediaData mediaData = new MediaData(title, artist, fileType, size, duration);
+                    mediaDatas.Add(mediaData);
+                }
+            }
+            return mediaDatas;
             // Pour la prochaine fois, utilisez les lignes 37 à 49 pour faire cette méthode (comme PlotThatLine) qui va récupérer tout les files du chemin pour les afficher ensuite
+        }
+
+        private void ListeFichiersLocaux_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
