1b4c6cafc1e6ac1ba4802691f07fdc2eaa6544b8
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-12-30 16:25:01 +0100

    Refactor : (MediaData) Changement du nommage
    5m-DONE
    La classe a changé de nommage donc j'ai du suivre

--- P_BitRuisseau/MediaData.cs
+++ P_BitRuisseau/MediaData.cs
@@ -13,34 +13,27 @@ namespace P_BitRuisseau
         private string _file_artist;
         private string _file_type;
         private long _file_size;
-        private long _file_sizeAccurate;
         private string _file_duration;
-        private int _file_hash;
-        public MediaData(string file_name, string file_artist, string file_type, long file_size,long file_sizeAccurate, string file_duration)
+        public MediaData() { }
+
+        public MediaData(string file_name, string file_artist, string file_type, long file_size, string file_duration)
         {
-            File_name = file_name;
-            File_artist = file_artist;
-            File_type = file_type;
-            File_size = file_size;
-            File_duration = file_duration;
-            File_sizeAccurate = file_sizeAccurate;
+            this.Title = file_name;
+            this.Artist = file_artist;
+            this.Type = file_type;
+            this.Size = file_size;
+            this.Duration = file_duration;
         }
 
-        public string File_name { get => _file_name; set => _file_name = value; }
-        public string File_artist { get => _file_artist; set => _file_artist = value; }
-        public string File_type { get => _file_type; set => _file_type = value; }
-        public long File_size { get => _file_size; set => _file_size = value; }
-        public string File_duration { get => _file_duration; set => _file_duration = value; }
-        public int File_hash { get => _file_hash; set => _file_hash = value; }
-        public long File_sizeAccurate { get => _file_sizeAccurate; set => _file_sizeAccurate = value; }
+        public string Title { get => _file_name; set => _file_name = value; }
+        public string Artist { get => _file_artist; set => _file_artist = value; }
+        public string Type { get => _file_type; set => _file_type = value; }
+        public long Size { get => _file_size; set => _file_size = value; }
+        public string Duration { get => _file_duration; set => _file_duration = value; }
 
-        private void createHash()
-        {
-            File_hash = this.GetHashCode();
-        }
         public string EncodeFileToBase64()
         {
-            byte[] fileBytes =  System.IO.File.ReadAllBytes( $"../../../ressource/{this.File_name} - {this.File_artist}.mp3");
+            byte[] fileBytes =  System.IO.File.ReadAllBytes( $"../../../ressource/{this.Title} - {this.Artist}{this.Type} ");
 
 
             return Convert.ToBase64String(fileBytes);
